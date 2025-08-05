8127da31e6d41782e602d290b980e723684294b1
Devprodipraj <devprodipraj@gmail.com>
2025-08-05 21:09:16 +0600

    complete whats is start kit

--- src/js/script.js
+++ src/js/script.js
@@ -129,21 +129,7 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("scroll", handleScrollAnimations);
   handleScrollAnimations();
 });
-
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute("href"));
-    if (target) {
-      const offsetTop = target.offsetTop - 70;
-      window.scrollTo({
-        top: offsetTop,
-        behavior: "smooth",
-      });
-    }
-  });
-});
-
+ 
 // product slider
 
   class ProductSlider {
